1d291083a3d39fa15511c3aa5bbcd4675e29fabd
chmo <christian.modery@exxeta.com>
2018-10-31 01:20:29 +0100

    Additional AR Tests
    Added further Hiro Marker HTML Files:
    - File with four simple distinct objects
    - File with click-event that increases object

---
+++ marker-events-handler.js
@@ -0,0 +1,19 @@
+// Copied from "https://rawgit.com/nicolocarpignoli/nicolocarpignoli.github.io/master/ar-playground/events.js"
+
+// register our component
+AFRAME.registerComponent('markerhandler', {
+
+    init: function() {
+        const animatedMarker = document.querySelector("#animated-marker");
+
+        animatedMarker.addEventListener('click', function(ev){
+            if (animatedMarker.object3D.visible == true && ev.detail.cursorEl) {
+                const entity = document.querySelector('#animated-model');
+                const scale = entity.getAttribute('scale');
+                Object.keys(scale).forEach((key) => scale[key] = scale[key] + 1);
+                
+                // every click, we make our model grow in size
+                entity.setAttribute('scale', scale);
+            }
+        });
+}});
